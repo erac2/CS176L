352de3a82a31b4c0c218a12f8f22911d68f99d69
erac2 <71734077+erac2@users.noreply.github.com>
2021-10-28 15:53:17 -0400

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,33 @@
+/**
+ * Define the Person superclass.
+ * @author linuxuser
+ *
+ */
+
+public class Person {
+	// data
+	private String name;
+	
+	// methods
+	/**
+	 * set Person's name
+	 */
+	public void setName(String name) 
+	
+	{
+		this.name = name;
+	}
+
+	/**
+	 * Return the Person's class and name.
+	 */
+	
+	public String toString()
+	{
+		
+		String class1= Person.class;
+		return name;
+	}
+	
+	
+}
